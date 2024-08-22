075e2ddf2340b31d02359be6c02ad0b69bb728d3
Taiki Sugawara <buzz.taiki@gmail.com>
2024-08-23 05:38:14 +0900

    helloRoutes

--- ts/fastify/index.ts
+++ ts/fastify/index.ts
@@ -1,19 +1,27 @@
 // Import the framework and instantiate it
-import Fastify from "fastify";
+import Fastify, { type FastifyInstance } from "fastify";
 
-const fastify = Fastify({
-  logger: true,
-});
-
-// Declare a route
-fastify.get("/", async function handler(_request, _reply) {
-  return { hello: "world" };
-});
-
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 });
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
+function helloRoutes(fastify: FastifyInstance) {
+  // Declare a route
+  fastify.get("/", async function handler(_request, _reply) {
+    return { hello: "world" };
+  });
 }
+
+async function main() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  helloRoutes(fastify);
+
+  // Run the server!
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+await main();
