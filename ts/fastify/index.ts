3e2325fffc7eb9a4070de5dc667614bd48875b2b
Taiki Sugawara <buzz.taiki@gmail.com>
2024-08-23 05:36:01 +0900

    fastify hello world

--- ts/fastify/index.ts
+++ ts/fastify/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+// Import the framework and instantiate it
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(_request, _reply) {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
