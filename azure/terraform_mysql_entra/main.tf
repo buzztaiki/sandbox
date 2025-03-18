d1bebc24d4645263d2830428c4887e3100eacf2c
Taiki Sugawara <buzz.taiki@gmail.com>
2025-03-18 21:37:01 +0900

    [terraform_mysql_entra] add mysql_identity_grant_directory_readers option

--- azure/terraform_mysql_entra/main.tf
+++ azure/terraform_mysql_entra/main.tf
@@ -99,14 +99,26 @@ data "azuread_service_principal" "msgraph" {
 }
 
 resource "azuread_app_role_assignment" "this" {
-  for_each = toset([
+  # var.mysql_identity_grant_directory_readers が false の場合だけ有効
+  for_each = toset([for x in [
     "User.Read.All",
     "GroupMember.Read.All",
     "Application.Read.All",
-  ])
+  ] : x if !var.mysql_identity_grant_directory_readers])
   app_role_id         = data.azuread_service_principal.msgraph.app_role_ids[each.value]
   principal_object_id = azurerm_user_assigned_identity.this.principal_id
   resource_object_id  = data.azuread_service_principal.msgraph.object_id
+}
+
+resource "azuread_directory_role" "directory_readers" {
+  display_name = "Directory Readers"
+}
+
+resource "azuread_directory_role_assignment" "this" {
+  # var.mysql_identity_grant_directory_readers が true の場合だけ有効
+  count               = var.mysql_identity_grant_directory_readers ? 1 : 0
+  role_id             = azuread_directory_role.directory_readers.template_id
+  principal_object_id = azurerm_user_assigned_identity.this.principal_id
 }
 
 resource "azuread_group" "mysql_admin" {
--- azure/terraform_mysql_entra/variables.tf
+++ azure/terraform_mysql_entra/variables.tf
@@ -24,3 +24,14 @@ variable "mysql_audit_log_events" {
   type    = string
   default = "CONNECTION,DDL,DCL,DML"
 }
+
+variable "mysql_identity_grant_directory_readers" {
+  type        = bool
+  default     = false
+  description = <<EOT
+MySQL の UAI への権限として Directory Readers を与えるかどうか。
+
+- false: 最低限必要な Microsoft Graph Role を個別に与える。
+- true: Directory Readers を与える。
+  EOT
+}
