d34b49f03b676fa0ff9fe873bae9a5c9ee67e2ae
Taiki Sugawara <buzz.taiki@gmail.com>
2025-07-13 07:37:57 +0900

    poetry -> uv

--- azure/azure-mlflow-inspect/inspect_run.py
+++ azure/azure-mlflow-inspect/inspect_run.py
@@ -5,24 +5,23 @@ from dataclasses import dataclass
 import mlflow
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
-from mlflow.store.tracking.rest_store import RunInfo
 
 
 @dataclass
 class Args:
     config: str
-    run: str
+    run_name: str
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="path to config json", default="config.json")
-    parser.add_argument("run", help="run to inspect")
+    parser.add_argument("run_name", help="run to inspect")
 
     args = parser.parse_args()
     return Args(
         config=args.config,
-        run=args.run,
+        run_name=args.run_name,
     )
 
 
@@ -31,16 +30,19 @@ def main():
     ml_client = MLClient.from_config(DefaultAzureCredential(), file_name=args.config)
     ws = ml_client.workspaces.get(ml_client.workspace_name)
 
+    assert ws is not None
+    assert ws.mlflow_tracking_uri is not None
+
     # Note: we must use set_tracking_uri to set tracking and registry uri with azureml-mlflow extension
     mlflow.set_tracking_uri(ws.mlflow_tracking_uri)
-    mlflow_client = mlflow.tracking.MlflowClient()
-    run = mlflow_client.get_run(args.run)
+    mlflow_client = mlflow.MlflowClient()
+    run = mlflow_client.get_run(args.run_name)
     print("RUN:")
     pprint.pp(run.to_dictionary())
     print()
 
     print("ARTIFACTS:")
-    run_info: RunInfo = run.info
+    run_info = run.info
     for artifact in mlflow_client.list_artifacts(run_info.run_id):
         print(artifact)
     print()
