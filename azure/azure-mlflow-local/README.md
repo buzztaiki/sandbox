4b2dd7e55440d0f80b6f19c027df7fc46687d0d5
Taiki Sugawara <buzz.taiki@gmail.com>
2025-07-13 07:14:50 +0900

    delete unmaintained packages

--- azure/azure-mlflow-local/README.md
+++
@@ -1,4 +0,0 @@
-以下をやっておく
-```
-MLFLOW_TRACKING_URI=<Azure Portal から取れるMLflow 追跡 URI>
-```
--- python/mlflow-quickstart/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [
-      pkgs.llvmPackages_10.libllvm
-      pkgs.llvmPackages_10.clang
-      pkgs.python3
-      pkgs.poetry
-    ];
-}
