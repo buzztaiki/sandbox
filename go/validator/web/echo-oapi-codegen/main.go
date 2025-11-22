ed51c241d1d4573bc437732ab972522fcd5d87a5
Taiki Sugawara <buzz.taiki@gmail.com>
2025-11-22 20:06:07 +0900

    validation

--- go/validator/web/echo-oapi-codegen/main.go
+++ go/validator/web/echo-oapi-codegen/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"github.com/buzztaiki/sandbox/go/validator/web/echo-oapi-codegen/petstore"
 	"github.com/labstack/echo/v4"
+	echomiddleware "github.com/oapi-codegen/echo-middleware"
 )
 
 func main() {
 	e := echo.New()
-	petstore.RegisterHandlers(e, NewPetHandler())
+
+	swagger, err := petstore.GetSwagger()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	e.Use(echomiddleware.OapiRequestValidator(swagger))
+	petstore.RegisterHandlersWithBaseURL(e, NewPetHandler(), "/v3")
 	e.Logger.Fatal(e.Start(":1323"))
 }
