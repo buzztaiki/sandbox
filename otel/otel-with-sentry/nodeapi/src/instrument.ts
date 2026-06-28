c5bad44c8ddf04682f24fd49e82518744e82dd25
Taiki Sugawara <buzz.taiki@gmail.com>
2026-06-29 03:21:41 +0900

    nodeapi: sentry が otel span を作ったり pipeline を触ったりしないようにする

--- otel/otel-with-sentry/nodeapi/src/instrument.ts
+++ otel/otel-with-sentry/nodeapi/src/instrument.ts
@@ -1,6 +1,9 @@
 import * as Sentry from "@sentry/hono/node";
+import type { NodeOptions } from "@sentry/node";
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 0.0,
+  skipOpenTelemetrySetup: true,
+  tracesSampleRate: undefined,
   dataCollection: {},
-});
+} as NodeOptions); // sentry/hono/node には skipOpenTelemetrySetup が存在しない
